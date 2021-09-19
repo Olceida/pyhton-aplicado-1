dfa5e6c2a6697b055b099197ccdafd8f4bc4402d
olceida <alexisolceida@gmail.com>
2021-09-18 21:07:06 -0500

    aaa

---
+++ funciones.py
@@ -0,0 +1,11 @@
+def suma(*numeros):
+    resultado = 0
+    for numero in numeros:
+        resultado = resultado+numero
+    return resultado
+   
+def funcionBasura1():
+    pass
+    
+def funcionBasura2():
+    pass
